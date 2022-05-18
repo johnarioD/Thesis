684d364e1602a59484ac6c2a71d49790d124d0fa
johnarioD <65023183+johnarioD@users.noreply.github.com>
2022-05-18 09:03:38 +0300

    Initial Commit

--- model.py
+++ model.py
@@ -1,14 +1,7 @@
-import mlflow
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
-from torchvision.datasets import MNIST
-from torchvision import transforms
 import pytorch_lightning as pl
-import mlflow.pytorch as tracker
-from mlflow.tracking import MlflowClient
 
 
 class LitAutoEncoder(pl.LightningModule):
